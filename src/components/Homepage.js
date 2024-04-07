207e6ac69a6a2eb27fea12128e013b2a39f960bd
dt3280097@gmail.com <dt3280097@gmail.com>
2024-04-07 18:14:19 +0545

    changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -14,7 +14,10 @@ function Homepage() {
             <button onClick={() => changeLang("en")} className='en m-2'>Eng</button>
             <button onClick={() => changeLang("ch")} className='ch m-2'>Ch</button>
 
-            <h2>{t("greeting")} hello</h2>
+            <h2>{t("greeting")} </h2>
+            <h2>{t("farewell")} </h2>
+            <h2>{t("thank_you")} </h2>
+
         </>
     );
 }
